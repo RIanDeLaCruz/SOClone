83a7f9a27028d19c61d5a12511a99588221df075
Ian De La Cruz <ridlc10@gmail.com>
2016-06-09 10:25:15 +0800

    User model with password authentication with bcrypt

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email:"a@example.com")
+    @user = User.new(name: "Example User", email:"a@example.com", password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do 
@@ -22,7 +22,17 @@ class UserTest < ActiveSupport::TestCase
   test "email should be unique" do
     duplicate_user = @user.dup
     @user.save
-    asser_not duplicate_user.valid?
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 
   # test "the truth" do
