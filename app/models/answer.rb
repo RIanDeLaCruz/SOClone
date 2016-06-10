ddde523983bc8532a7400a1df1a36c3db67012b6
Ian De La Cruz <ridlc10@gmail.com>
2016-06-10 15:34:23 +0800

    Cleaned Show View for Question, removing references to Comment

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,7 @@ class Answer < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
 
-  has_many :answers, :class_name => "Answer", :foreign_key => "comments"
+  has_many :comments, :class_name => "Answer", :foreign_key => "comments"
+  belongs_to :parent_answer, :class_name => "Answer"
   has_many :votes, :as => :votable
 end
